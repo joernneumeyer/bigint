2b24976f60ec8093af68a86d8b0309b142631196
Jörn Neumeyer <contact@joern-neumeyer.de>
2019-10-31 14:45:17 +0100

    updated testing utilities

--- test.cpp
+++ test.cpp
@@ -3,18 +3,38 @@
 
 #include "bigint.hpp"
 
+void assert(const std::string &name, bool condition, std::initializer_list<std::string> error_messages = { }) {
+  std::cout << "testing '" << name << "': ";
+  if (condition) {
+    std::cout << "successful";
+  } else {
+    std::cout << "failed! Message: ";
+    if (error_messages.size()) for (const auto &a : error_messages) std::cout << a << ' ';
+    else std::cout << "[none]";
+  }
+  std::cout << std::endl;
+}
+
+template <typename T>
+void assertEqual(const std::string &name, T expected, T actual) {
+  assert(name, expected == actual, { "Expected", expected, "but received", actual, "instead!" });
+}
+
 int main() {
-  std::cout << "equal:      " << (std::strcmp(jn::big("200").to_string().c_str(), "200") == 0) << std::endl;
-  std::cout << "larger:     " << (jn::big("65436541") > "9875")                                << std::endl;
-  std::cout << "!larger:    " << (!(jn::big("30") > "9875"))                                   << std::endl;
-  std::cout << "1 smaller:  " << (jn::big("564") < "9875")                                     << std::endl;
-  std::cout << "2 smaller:  " << (jn::big("56444") < "98754")                                  << std::endl;
-  std::cout << "1!smaller:  " << (!(jn::big("35480") < "9875"))                                << std::endl;
-  std::cout << "2!smaller:  " << (!(jn::big("1654654") < "98754"))                             << std::endl;
-  std::cout << "equal:      " << (jn::big("65436541") == "65436541")                           << std::endl;
-  std::cout << "not equal:  " << (jn::big("65436541") != "75436541")                           << std::endl;
-  std::cout << "add:        " << (jn::big("555").add("445") == "1000")                         << std::endl;
-  std::cout << "subtract:   " << (jn::big("1000").subtract("454") == "546")                    << std::endl;
-  std::cout << "1 multiply: " << (jn::big("12").multiply("12") == "144")                       << std::endl;
-  std::cout << "2 multiply: " << (jn::big("85492").multiply("751684") == "64262968528")        << std::endl;
+  assert("equal", jn::big("200") == "200", { "Expected equality!" });
+  assert("larger", jn::big("65436541") > "9875", { "Should work!" });
+  assert("!larger", !(jn::big("30") > "9875"), { "Should work!" });
+  assert("1 smaller", jn::big("564") < "9875", { "Should work!" });
+  assert("2 smaller", jn::big("56444") < "98754", { "Should work!" });
+  assert("!1 smaller", !(jn::big("35480") < "9875"), { "Should work!" });
+  assert("!2 smaller", !(jn::big("1654654") < "98754"), { "Should work!" });
+  assert("1 smaller", jn::big("564") < "9875", { "Should work!" });
+  assertEqual<std::string>("equal", "65436541", jn::big("65436541"));
+  assertEqual<std::string>("not equal", "65436541", jn::big("65436541"));
+  assertEqual<std::string>("add", "1000", jn::big("555").add("445"));
+  assertEqual<std::string>("subtract", "546", jn::big("1000").subtract("454"));
+  assertEqual<std::string>("multiply", "144", jn::big("12").multiply("12"));
+  assertEqual<std::string>("multiply", "64262968528", jn::big("85492").multiply("751684"));
+  assertEqual<std::string>("add", "-400", jn::big("400").add("-800"));
+  assertEqual<std::string>("add", "420", jn::big("-400").add("820"));
 }
